086884c7115fffd39164d86436b803b6bf12d8c6
Alexander <alex.martirosyan.04@gmail.com>
2024-04-02 14:58:59 +0200

    Adds a menu for multiplayer and single player mode

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,14 +2,49 @@ package main;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-public class Main {
+public class Main extends JFrame {
+    private JButton singlePlayerButton;
+    private JButton multiplayerButton;
 
-    public static void main(String[] args) {
+    public Main() {
+        setSize(200, 200);
+        setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel(new GridLayout(2, 1));
+
+        singlePlayerButton = new JButton("Single Player");
+        multiplayerButton = new JButton("Multiplayer");
+
+        singlePlayerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                startSinglePlayerGame();
+                dispose();
+            }
+        });
+
+        multiplayerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                startMultiplayerGame();
+                dispose();
+            }
+        });
+
+        panel.add(singlePlayerButton);
+        panel.add(multiplayerButton);
+
+        add(panel);
+        setVisible(true);
+    }
 
 
+    public void startSinglePlayerGame() {
         JFrame frame = new JFrame();
         frame.getContentPane().setBackground(Color.BLACK);
         frame.setLayout(new GridBagLayout());
@@ -19,7 +54,6 @@ public class Main {
         Board board = new Board();
         frame.add(board);
 
-
         frame.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -28,6 +62,21 @@ public class Main {
         });
 
         frame.setVisible(true);
+    }
+
+    public void startMultiplayerGame() {
+        // TODO: Implement multiplayer game logic
+    }
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Main();
+            }
+        });
 
     }
+
 }
